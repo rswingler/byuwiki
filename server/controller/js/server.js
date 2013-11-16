f0a2c47a4ceed89ce78570ec51409c779b8888b3
Stephen Rollins <stephen@lucidchart.com>
2013-11-16 14:46:43 -0700

    Added Express as a dependency for the server and set up the basic server file to use Express

--- server/controller/js/server.js
+++ server/controller/js/server.js
@@ -1,14 +1,24 @@
 // Primary Node.js server file
 
 var run = function(port) {
-	var http = require('http');
-	http.createServer(function (req, res) {
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('BYU Class wiki, served from nine lines of code with Node.js.\n');
+	var express = require('express');
+	var app = express();
 
-	}).listen(port);
+	app.get('/', function(req, res) {
+		// res.writeHead(200, {'Content-Type': 'text/plain'});
+		res.send('BYU Class wiki, served from nine lines of code with Node.js.\n');
+	});
+
+	app.listen(port);
+
+	// var http = require('http');
+	// http.createServer(function (req, res) {
+	// 	res.writeHead(200, {'Content-Type': 'text/plain'});
+	// 	res.end('BYU Class wiki, served from nine lines of code with Node.js.\n');
+
+	// }).listen(port);
 
 	console.log('Server listening on port ' + port);
-}
+};
 
 exports.run = run;
