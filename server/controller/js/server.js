d6c254fdf1178350c6210ee99854ecb80b549283
Stephen Rollins <stephen@lucidchart.com>
2013-11-16 14:58:42 -0700

    Cleaned up some unnecessary comments and updated the version of the projects to 0.0.2

--- server/controller/js/server.js
+++ server/controller/js/server.js
@@ -5,18 +5,10 @@ var run = function(port) {
 	var app = express();
 
 	app.get('/', function(req, res) {
-		// res.writeHead(200, {'Content-Type': 'text/plain'});
 		res.send('BYU Class wiki, served from nine lines of code with Node.js.\n');
 	});
 
 	app.listen(port);
-
-	// var http = require('http');
-	// http.createServer(function (req, res) {
-	// 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	// 	res.end('BYU Class wiki, served from nine lines of code with Node.js.\n');
-
-	// }).listen(port);
 
 	console.log('Server listening on port ' + port);
 };
