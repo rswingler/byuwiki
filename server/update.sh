2c606ddc86de694fbc163f5e708ca0ac9a5d10f6
Stephen Rollins <stephen@lucidchart.com>
2013-11-16 19:30:41 -0700

    Updated the pull script again

--- server/update.sh
+++ server/update.sh
@@ -6,5 +6,4 @@ echo "update.sh - Pulling from github and restarting the server (at $(date))" >>
 #git stash >> output.log
 git pull --rebase origin master >> output.log
 #git stash pop >> output.log
-sudo forever stop controller/js/wiki.js >> output.log
-sudo forever start controller/js/wiki.js >> output.log
+sudo forever restart controller/js/wiki.js >> output.log
