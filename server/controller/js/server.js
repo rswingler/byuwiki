3e0be3e68e4644b39cae52f4bf36a9552955ff76
Stephen Rollins <stephen@lucidchart.com>
2013-11-16 19:29:16 -0700

    Another commit to test the auto pull script

--- server/controller/js/server.js
+++ server/controller/js/server.js
@@ -42,7 +42,7 @@ var run = function(port) {
 	 * file.
 	 */
 	var homepage = function(req, res) {
-		res.send('BYU Class wiki, served direct to you from Node.js!');
+		res.send('BYU Class wiki, served direct to you from Node.js!!');
 	};
 
 	/**
