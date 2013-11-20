0e95eab2708585f52965e9d25d8c0a8b10e0fe01
rswingler <bobbyswingler@gmail.com>
2013-11-20 14:41:33 -0700

    Update server.js

--- server/controller/js/server.js
+++ server/controller/js/server.js
@@ -56,7 +56,7 @@ var run = function(port) {
 		console.log('Pulling the latest code from github');
 		res.send('Please verify server has run update.sh');
 		spawnOne('./update.sh');
-		spawnOne('sudo ./restart.sh');
+		spawnTwo('sudo ./restart.sh');
 
 
 	};
