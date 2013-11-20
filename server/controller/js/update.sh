b31268a69a038733bcb509289ddd40d17cba1190
rswingler <bobbyswingler@gmail.com>
2013-11-20 14:32:44 -0700

    Update update.sh

--- server/controller/js/update.sh
+++ server/controller/js/update.sh
@@ -4,8 +4,8 @@ echo "===============================" >> output.log
 echo "update.sh - Pulling from github and restarting the server (at $(date))" >> output.log
 
 #git stash >> output.log
-sudo git pull --rebase origin master >> output.log
+git pull --rebase origin master >> output.log
 #git stash pop >> output.log
 #sudo forever restart controller/js/wiki.js >> output.log
 #wait
-sudo forever restart wiki.js >> output.log
+#sudo forever restart wiki.js >> output.log
