921b1092399a2f084295783b019bcb397cb806b4
Bobby Swingler <bobbyswingler@gmail.com>
2013-11-20 14:33:29 -0700

    adding secondary restart script

---
+++ server/controller/js/restart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "===============================" >> output.log
+echo "update.sh - Pulling from github and restarting the server (at $(date))" >> output.log
+
+#git stash >> output.log
+# git pull --rebase origin master >> output.log
+#git stash pop >> output.log
+#sudo forever restart controller/js/wiki.js >> output.log
+#wait
+sudo forever restart wiki.js >> output.log
