8f631ade1338a263af32a1e5bd4015f5c33f5d83
Bobby Swingler <bobbyswingler@gmail.com>
2013-11-20 14:20:19 -0700

    modified server.js, removed second spawn reference

--- server/controller/js/server.js
+++ server/controller/js/server.js
@@ -49,13 +49,13 @@ var run = function(port) {
 	 * Runs a bash script that pulls from the github repository and reloads the server
 	 */
 	var updateRepository = function(req, res) {
-		var spawnOne = require('child_process').spawn;
-		var spawnTwo = require('child_process').spawn;
+		var spawn = require('child_process').spawn;
+
 
 		console.log('Pulling the latest code from github');
 		res.send('Please verify server has run update.sh');
-		spawnOne('sudo ./update.sh');
-		spawnTwo('sudo ./restart.sh');
+		spawn('sudo ./update.sh');
+
 
 	};
 
