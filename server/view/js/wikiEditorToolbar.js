57a3744a749326b17ff2c751a72b066e1960bea1
Bobby Swingler <bobbyswingler@gmail.com>
2013-12-16 15:42:27 -0700

    Added POST functionality to editor

--- server/view/js/wikiEditorToolbar.js
+++ server/view/js/wikiEditorToolbar.js
@@ -64,12 +64,6 @@ function addImage()
     
 }
 
-//INPUT TABLE
-function addTable()
-{
- 	
-}
-
 function addMarkup(el,markup)
 {
     if (typeof el.selectionStart == "number") 
@@ -124,6 +118,69 @@ function renderHTML()
 
 	//alert(html);
 	previewPane.innerHTML = html;
+}
+
+function saveAndPost()
+{
+   //GET DOCUMENT ELEMENTS
+   var inputPane = document.getElementById('inputPane');
+   var previewPane = document.getElementById('previewPane');
+   var location = document.location.href;
+   var urlArray = location.split("/");
+
+   //GET TITLE, MARKUP, AND HTML CONTENT
+   var title = urlArray[urlArray.length - 1];
+   var markupContent = inputPane.value;
+   var htmlContent = previewPane.innerHTML;
+
+   //BUILD JSON
+   var article = new Object();
+   article.wikiTitle = title;
+   article.markup = markupContent;
+   article.html = htmlContent;
+   var jsonText = JSON.stringify(article);
+
+   //PREP POST
+   var postURL = "http://ec2-54-201-62-212.us-west-2.compute.amazonaws.com/update/" + title;
+   var form = document.createElement("form");
+   form.setAttribute("method", "post");
+   form.setAttribute("action", postURL);
+
+   //PREP FORM
+   var hiddenField = document.createElement("input");
+   hiddenField.setAttribute("type", "hidden");
+   hiddenField.setAttribute("name", "jsonArticle");
+   hiddenField.setAttribute("value", jsonText);
+   form.appendChild(hiddenField);
+
+   //APPEND FORM TO DOC, SUBMIT FORM VIA POST
+   document.body.appendChild(form);
+   form.submit();
+
+   //alert("TITLE: " + title);
+   //alert("MARKUP: "+markupContent);
+   //alert("HTML: " + htmlContent);
+   //alert("JSON: " + jsonText);
+}
+
+function send() {
+        var person = {
+            name: $("#id-name").val(),
+            address:$("#id-address").val(),
+            phone:$("#id-phone").val()
+        };
+
+        $('#target').html('sending..');
+
+        $.ajax({
+            url: '/test/PersonSubmit',
+            type: 'post',
+            dataType: 'application/json',
+            success: function (data) {
+                $('#target').html(data.msg);
+            },
+            data: person
+        });
 }
 
 // function getSelectedText(el) {
